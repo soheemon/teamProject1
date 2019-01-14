734e0a28a98a1bcaabf9ffb304565ee649af02f6
tncnsfs@gmail.com <tncnsfs@gmail.com>
2019-01-14 10:54:59 +0900

    test success Push from gitSourceTree

--- kogile/src/testpack/TestClass.java
+++ kogile/src/testpack/TestClass.java
@@ -4,7 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TestClass001");
+		System.out.println("TestClass004");
+		System.out.println("TestClass012");
 
 	}
 
