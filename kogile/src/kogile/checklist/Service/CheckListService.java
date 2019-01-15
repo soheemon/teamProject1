73fce627557efd40341727c982b613f7d9e207bb
tncnsfs@gmail.com <tncnsfs@gmail.com>
2019-01-15 16:25:55 +0900

    insert commit 16:25

--- kogile/src/kogile/checklist/Service/CheckListService.java
+++ kogile/src/kogile/checklist/Service/CheckListService.java
@@ -19,15 +19,15 @@ public class CheckListService {
 
 		
 		Board board = new Board();
-		board.setChecklist_no(Integer.parseInt(request.getParameter("checklist_no")));
+//		board.setChecklist_no(Integer.parseInt(request.getParameter("checklist_no")));
 		
 		board.setCheck_title(request.getParameter("check_title"));
-		board.setP_no(Integer.parseInt(request.getParameter("p_no")));
+//		board.setP_no(Integer.parseInt(request.getParameter("p_no")));
 		
 		
-		System.out.println("board.getChecklist_no() :" + board.getChecklist_no());
+//		System.out.println("board.getChecklist_no() :" + board.getChecklist_no());
 		System.out.println("board.getCheck_title() :" + board.getCheck_title());
-		System.out.println("board.getP_no() :" + board.getP_no());
+//		System.out.println("board.getP_no() :" + board.getP_no());
 //		dao.insertchecklist(board);
 		return dao.insertchecklist(board);
 	}
