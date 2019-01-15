fd0a78e6eeb53eb6d40db015128f20c130209a62
tncnsfs@gmail.com <tncnsfs@gmail.com>
2019-01-15 19:33:13 +0900

    11111111111111111

--- kogile/src/kogile/checklist/action/Action.java
+++ kogile/src/kogile/checklist/action/Action.java
@@ -5,6 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
 	
-	public ActionForward excute(HttpServletRequest request, HttpServletResponse response)throws Exception; 
-	
+	public ActionForward excute(HttpServletRequest request, HttpServletResponse response)throws Exception;
+
 }
